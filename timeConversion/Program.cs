ac743f113432c74d90c54a9e3ae6f59ebae41296
Sujith Acharya <sujith.acharya054@yahoo.com>
2019-02-05 21:53:24 +0530

    time conversion

---
+++ timeConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static string timeConversion(string s)
+    {
+        DateTime d = DateTime.Parse(s);
+        return d.ToString("HH:mm:ss");
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Provide Input:");
+        string s = Console.ReadLine();
+        string result = timeConversion(s);
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
